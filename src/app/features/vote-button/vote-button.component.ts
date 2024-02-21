0b85b69ff512f58edc6d98a3c35b24c6ce5ca769
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-02-21 10:50:54 +0100

    create vote button component

---
+++ src/app/features/vote-button/vote-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vote-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './vote-button.component.html',
+  styleUrl: './vote-button.component.css'
+})
+export class VoteButtonComponent {
+
+}
