124ab6003f5c910356c2c3c9a65ee39e02b16e11
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-02-21 11:41:50 +0100

    create hero row component

---
+++ src/app/features/hero-row/hero-row.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-row',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-row.component.html',
+  styleUrl: './hero-row.component.css'
+})
+export class HeroRowComponent {
+
+}
