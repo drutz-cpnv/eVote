e87a52c5d2d0c03a04f8b20a785b31ce3e0e6a0a
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-02-21 11:53:26 +0100

    Update icon.component.ts

--- src/app/features/icon/icon.component.ts
+++ src/app/features/icon/icon.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-icon',
@@ -8,6 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './icon.component.css'
 })
 export class IconComponent {
-  constructor(public name: string) {
-  }
+  @Input() name!: string;
 }
