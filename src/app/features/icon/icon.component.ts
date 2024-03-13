8d5ebdb1b3d2f9ed4a28a34dce6e839ad135e13a
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-03-13 11:17:28 +0100

    update icon component to take css class

--- src/app/features/icon/icon.component.ts
+++ src/app/features/icon/icon.component.ts
@@ -1,12 +1,14 @@
-import {Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, input, Input} from '@angular/core';
 
 @Component({
   selector: 'app-icon',
   standalone: true,
   imports: [],
   templateUrl: './icon.component.html',
-  styleUrl: './icon.component.css'
+  styleUrl: './icon.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class IconComponent {
-  @Input() name!: string;
+  @Input({required: true}) name: string = "";
+  @Input() styleClass: string = "text-2xl";
 }
