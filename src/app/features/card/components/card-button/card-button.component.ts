139216e45feb882c55df9f97388177249ab1620e
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-03-13 11:52:36 +0100

    put card button inside card componenet

--- src/app/features/card/components/card-button/card-button.component.ts
+++ src/app/features/card/components/card-button/card-button.component.ts
@@ -1,14 +1,16 @@
 import {Component, Input} from '@angular/core';
 import {IconComponent} from "../../../icon/icon.component";
 import { RouterLink } from '@angular/router';
+import {CardComponent} from "../../card.component";
 
 @Component({
   selector: 'app-card-button',
   standalone: true,
-    imports: [
-        IconComponent,
-        RouterLink
-    ],
+  imports: [
+    IconComponent,
+    RouterLink,
+    CardComponent
+  ],
   templateUrl: './card-button.component.html',
   styleUrl: './card-button.component.css'
 })
@@ -16,6 +18,4 @@ export class CardButtonComponent {
   @Input() iconName!: string;
   @Input() text!: string;
   @Input() linkPath!: string;
-
-
 }
