590d328f77ff9050a78946039df82636f91841e6
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-03-13 11:18:07 +0100

    create card button component

---
+++ src/app/features/card-button/card-button.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input} from '@angular/core';
+import {IconComponent} from "../icon/icon.component";
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-card-button',
+  standalone: true,
+    imports: [
+        IconComponent,
+        RouterLink
+    ],
+  templateUrl: './card-button.component.html',
+  styleUrl: './card-button.component.css'
+})
+export class CardButtonComponent {
+  @Input() iconName!: string;
+  @Input() text!: string;
+  @Input() linkPath!: string;
+
+
+}
