063ee933c8a1fbafe53a9c854975f0ba68dd9588
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-03-20 09:01:24 +0100

    feat : create new admin page component

--- src/app/features/adm-page/adm-page.component.ts
+++ src/app/features/adm-page/adm-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {CardButtonComponent} from "../card/components/card-button/card-button.component";
+import {HeaderComponent} from "../header/header.component";
 
 @Component({
   selector: 'app-adm-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CardButtonComponent,
+    HeaderComponent
+  ],
   templateUrl: './adm-page.component.html',
   styleUrl: './adm-page.component.css'
 })
