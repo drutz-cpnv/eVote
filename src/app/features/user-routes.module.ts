4ac69f1cf3151cd7cde9c1c2b9cc875c05d91ab1
Zwissig Noé <54308994+Witex15@users.noreply.github.com>
2024-03-20 09:00:51 +0100

    feat : add new routes

--- src/app/features/user-routes.module.ts
+++ src/app/features/user-routes.module.ts
@@ -1,14 +1,24 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {ComponentPageComponent} from "./component-page/component-page.component";
 import {MainPageComponent} from "./main-page/main-page.component";
+import {AdmPageComponent} from "./adm-page/adm-page.component";
+import {NewVotePageComponent} from "./new-vote-page/new-vote-page.component";
 
-export const routes:Routes = [
-    {
-        path: "components",
-        component: ComponentPageComponent
-    },
-    {
-        path:"",
-        component: MainPageComponent
-    }
+export const routes: Routes = [
+  {
+    path: "components",
+    component: ComponentPageComponent
+  },
+  {
+    path: "admin",
+    component: AdmPageComponent
+  },
+  {
+    path: "admin/vote/new",
+    component: NewVotePageComponent
+  },
+  {
+    path: "",
+    component: MainPageComponent
+  }
 ];
