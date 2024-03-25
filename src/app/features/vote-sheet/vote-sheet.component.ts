2a81aac5f22ba5671a21c30d64ae6c55bb5ff5d6
drutz-cpnv <dimitri.rutz@eduvaud.ch>
2024-03-25 19:03:53 +0100

    update

--- src/app/features/vote-sheet/vote-sheet.component.ts
+++ src/app/features/vote-sheet/vote-sheet.component.ts
@@ -1,19 +1,22 @@
 import {Component, Input} from '@angular/core';
 import {ObjectComponent} from "./object/object.component";
 import {Subject} from "../../../graphql/generated/graphql";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-vote-sheet',
   standalone: true,
   imports: [
-    ObjectComponent
+    ObjectComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './vote-sheet.component.html',
   styleUrl: './vote-sheet.component.css'
 })
 export class VoteSheetComponent {
   @Input() subjects: any;
-
+  user_id: FormControl = new FormControl();
+  value: FormControl<any> = new FormControl({})
 
   public getSubjects(): Array<Subject> {
     return this.subjects[0].federal_subject.subjects;
